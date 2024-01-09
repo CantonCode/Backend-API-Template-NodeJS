6d8506adaa31f348f31c1f8a778899a7114c84bb
Conor Canton <conorstreete@gmail.com>
2024-01-09 12:03:02 +0800

    created item schema/model

---
+++ app/models/Item/item.js
@@ -0,0 +1,19 @@
+const { string } = require('joi');
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const ItemSchema = new Schema({
+    name:{
+        type:String,
+        required:true,
+    },
+    created: { type: Date,
+         default: Date.now
+         },
+    itemTasks:{
+        type:Array,
+        required:true,
+    },
+});
+
+module.exports = mongoose.model('Job', JobSchema)
